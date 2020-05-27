b0014b55a230c026e5223d06a8f2b1d5bce0aa54
zinnourlahcene <zinnour@uqtr.ca>
2020-05-27 17:15:48 -0400

    recommendation

--- src/main/java/ca/uqtr/patient/dto/medicalfile/SocioDemographicVariablesDto.java
+++ src/main/java/ca/uqtr/patient/dto/medicalfile/SocioDemographicVariablesDto.java
@@ -12,7 +12,7 @@ public class SocioDemographicVariablesDto {
     private int age;
     private String civilStatus;
     private String gender;
-    private int familyIncome;
+    private String familyIncome;
     private String jobStatus;
     private String education;
     private LivingEnvironmentDto livingEnvironment;
